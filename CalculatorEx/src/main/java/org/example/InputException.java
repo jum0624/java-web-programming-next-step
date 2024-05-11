8772f8d83c1e5c0605c9b2af512b060b8ed39cfa
jum0624 <jum0624@naver.com>
2024-05-11 21:45:34 +0900

    feat: 문자열 계산기 덧셈기능 구현

---
+++ CalculatorEx/src/main/java/org/example/InputException.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class InputException extends RuntimeException {
+    public InputException() {
+        super();
+    }
+
+    public InputException(String message) {
+        super(message);
+    }
+
+    public InputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InputException(Throwable cause) {
+        super(cause);
+    }
+}
--- CalculatorEx/src/main/java/org/example/StringCalculator.java
+++ CalculatorEx/src/main/java/org/example/StringCalculator.java
@@ -1,21 +1,48 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private String text;
+    private String delimeter;
+
+    public StringCalculator() {
+        this.text = null;
+        this.delimeter = ":|,";
+    }
+
+    private boolean isEmptyText(String text) {
+        if (text == null || text.isBlank()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int stringToInt(String[] tokens) {
+        try {
+            return Arrays.stream(tokens)
+                    .mapToInt(Integer::parseInt)
+                    .filter(number -> number >= 0)
+                    .sum();
+        } catch (NumberFormatException e) {
+            throw new InputException("0 이상의 양수를 입력해주세요.");
+        }
+    }
+
+    public void customSplit(String text) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (matcher.find()) {
+            this.delimeter = matcher.group(1);
+            this.text = matcher.group(2);
+        }
+    }
 
     public int add(String text) {
-        if (text.isEmpty()) {
-            return 0;
-        }
-        try {
-            int number = Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            String[] split = text.split(",|:");
-            Matcher matcher = Pattern.compile("//(.*?)\\n", Pattern.DOTALL).matcher(text);  // "//"와 "\n" 사이의 값 추출
-
-        }
-        return 0;
+        if (isEmptyText(text)) return 0;
+        customSplit(text);
+        String[] tokens = this.text.split(delimeter);
+        return stringToInt(tokens);
     }
 }
